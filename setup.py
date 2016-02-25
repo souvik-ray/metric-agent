8699645ebb57dd4b4c4a069e428a8e8e9f82fd7e
souvik <souvik.ray@ril.com>
2016-02-26 01:00:19 +0530

    Add setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='metric-agent`',
+setup(name='metric-agent',
     version='0.1',
     description='Generic Metric Agent package which consumes generated metrics',
     url='http://github.com/JioCloud/metric-agent',
     author='Souvik Ray',
     author_email='souvik.ray@ril.com',
     license='Apache License 2.0',
-    packages=[],
     zip_safe=False)
