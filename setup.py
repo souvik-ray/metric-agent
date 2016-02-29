3664994fa9e5a118370d7e045f60aad99b27d4b8
souvik <souvik.ray@ril.com>
2016-02-29 14:39:22 +0530

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='metric-agent',
+setup(name='metricagent',
     version='0.1',
     description='Generic Metric Agent package which consumes generated metrics',
     url='http://github.com/JioCloud/metric-agent',
     author='Souvik Ray',
     author_email='souvik.ray@ril.com',
     license='Apache License 2.0',
+    packages=['metricagent'],
     zip_safe=False)
